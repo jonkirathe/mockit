707f2fc1628fbbb571288ad39fcdaac5c8fd1ccd
John Kirathe <jnkratz@gmail.com>
2025-03-07 13:06:01 +0300

    .env changes

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -30,7 +30,7 @@ const cookieOptions = {
 const allowedOrigins = [
     ...(process.env.ALLOWED_ORIGINS?.split(',') || []),
     ...(process.env.NODE_ENV === 'development'
-        ? ['http://localhost:3000', 'http://127.0.0.1:3000']
+        ? ['https://localhost:3000', 'http://127.0.0.1:3000']
         : [])
 ];
 
