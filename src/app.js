74589ac9a44ed568ec987c4925df0c690154cebc
John Kirathe <jnkratz@gmail.com>
2025-03-08 12:33:22 +0300

    solving deployment issues

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 import express, {Router} from "express";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
+import cors from "cors";
+import session from "express-session";
 import dotenv from "dotenv";
 import {PORT} from "./config/constants.js";
-// import {setupHelmet} from "./config/security.js";
-// import {apiLimiter} from "./config/rate-limits.js";
+import {setupHelmet} from "./config/security.js";
+import {apiLimiter} from "./config/rate-limits.js";
 import authRoutes from "./routes/auth.js";
 import petRoutes from "./routes/pets.js";
 import taskRoutes from "./routes/tasks.js";
 import userRoutes from "./routes/users.js";
-import * as rateLimits from "./config/rate-limits.js";
-const { apiLimiter } = rateLimits;
+import {generateCsrfToken} from "./utils/csrf.js";
 
 dotenv.config();
 
@@ -18,13 +19,12 @@ const api = express();
 api.set("port", PORT);
 
 // Security headers
-// setupHelmet(api);
+setupHelmet(api);
 
 api.use(express.json());
 api.use(express.urlencoded({ extended: true }));
 api.use(cookieParser());
 api.use(morgan("combined"));
-/*
 
 // CORS configuration: allow all origins in development; in production, enforce HTTPS
 api.use(
@@ -59,14 +59,13 @@ api.use((req, res, next) => {
     }
     next();
 });
-*/
 
 // Mount routes under /api
 const router = Router();
-// router.use("/auth", authRoutes);
-// router.use("/pets", petRoutes);
-// router.use("/tasks", taskRoutes);
-// router.use("/users", userRoutes);
+router.use("/auth", authRoutes);
+router.use("/pets", petRoutes);
+router.use("/tasks", taskRoutes);
+router.use("/users", userRoutes);
 
 router.get("/health", (req, res) => {
     res.status(200).json({message: "Health Ok"});
